d45079a6f5a6258e2fd6d1ddac3d896465244584
yashasvinitripathi11 <yashasvinitripathi@gmail.com>
2025-06-23 13:06:56 +0530

    796 leetcode

---
+++ Solution.java
@@ -0,0 +1,20 @@
+public class Solution {
+
+    public boolean rotateString(String s, String goal) {
+        if (s.length() != goal.length()) {
+            return false;
+        }
+        return (s + s).contains(goal);
+    }
+
+    public static void main(String[] args) {
+        Solution solution = new Solution();
+
+        // Test cases
+        String s = "abcde";
+        String goal = "cdeab";
+
+        boolean result = solution.rotateString(s, goal);
+        System.out.println("Is \"" + goal + "\" a rotation of \"" + s + "\"? " + result);
+    }
+}
